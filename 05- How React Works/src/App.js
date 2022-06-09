76791383996de24907bfb2541520f352fd4dca2d
prkm93 <aradshub93@gmail.com>
2022-06-09 09:16:30 +0530

    added some comments and descriptions for useCallback

--- 05- How React Works/src/App.js
+++ 05- How React Works/src/App.js
@@ -19,20 +19,41 @@ Component evaluation and component rendering are different things. Any console.l
 DOM element which is updated or added  is flashed in console elements tab.
 
  */
+
+
+/**
+ * 
+ * useCallback - this hook basically saves the function across component execution and ensures function shouldn’t be created with every execution. 
+In our case, it’s being used in conjunction with memo    and here.
+The “Button Running” isn’t executed every time a button is clicked because toggleParagraphHandler function is wrapped in useCallback and every time its new instance isn’t created on button click and remains the same.
+
+If we aren’t passing any dependency parameter in useCallback , then after allowToggle is changed, and component is re-executed, toggleParagraphHandler isn’t created again , so it still uses the old value of allowToggle , and not the updated value because value of allowToggle is assigned first time, when the function(toggleParagraphHandler) is created.
+
+When dependency parameter is passed, useCallback will recreate the function only when parameter is changed.
+
+ */
 function App() {
 
   const [togglePara, setTogglePara] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   console.log("App running");
 
   const toggleParagraphHandler = useCallback(() => {
-    setTogglePara(prevState  => !prevState);
-  }, []);
+    if (allowToggle) {
+      setTogglePara(prevState  => !prevState);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(prevState => !prevState);
+  }
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput  show={false}/> 
+      <DemoOutput  show={togglePara}/>
+      <Button onClick={allowToggleHandler}>Allow Toggling</Button>
       <Button onClick={toggleParagraphHandler}>Add paragraph</Button>
     </div>
   );
