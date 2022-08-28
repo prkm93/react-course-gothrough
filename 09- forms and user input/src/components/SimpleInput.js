45fec20e16ab5434e98dd1135e96af30b5d847e6
prkm93 <aradshub93@gmail.com>
2022-08-28 21:24:48 +0530

    added button disabling

--- 09- forms and user input/src/components/SimpleInput.js
+++ 09- forms and user input/src/components/SimpleInput.js
@@ -8,6 +8,12 @@ const SimpleInput = (props) => {
   const enteredNameIsValid = enteredName.trim() !== '';
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
+  let formIsValid = false;
+
+  if (enteredNameIsValid) {
+    formIsValid = true;
+  }
+
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
 
@@ -49,7 +55,7 @@ const SimpleInput = (props) => {
          {nameInputIsInvalid && <p className="error-text">Name mustn't be empty.</p>} 
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
